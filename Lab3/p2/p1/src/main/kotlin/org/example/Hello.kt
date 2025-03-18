748f0431c897d37c7ebfe7dce25079bf92d4c545
DariaAdelinne <157800209+DariaAdelinne@users.noreply.github.com>
2025-03-18 14:17:50 +0200

    Update Hello.kt

--- Lab3/p2/p1/src/main/kotlin/org/example/Hello.kt
+++ Lab3/p2/p1/src/main/kotlin/org/example/Hello.kt
@@ -2,15 +2,16 @@ package org.example
 
 import org.jsoup.Jsoup
 
-fun procesareJsoup(url: String): MutableMap<String, MutableMap<String, String>> {
-    val documentHtml = Jsoup.connect(url).get()
-    val lista = mutableMapOf<String, MutableMap<String, String>>()
+fun procesareJsoup(url: String): MutableMap<String, MutableMap<String, String>> //primeste string si returneaza MutableMap
+{
+    val documentHtml = Jsoup.connect(url).get() //se conecteaza la link
+    val lista = mutableMapOf<String, MutableMap<String, String>>() //creeaza afisarea
 
-    val iteme = documentHtml.select("item")
+    val iteme = documentHtml.select("item") //selecteaza fiecare item din url
     for (item in iteme) {
-        val titlu = item.select("title").text()
+        val titlu = item.select("title").text() //selecteaza titlul
 
-        val deAdaugat = mutableMapOf(
+        val deAdaugat = mutableMapOf( //creaza mutablemap cu informatiile necesare
             "Link" to item.select("link").text(),
             "Descriere" to item.select("description").text(),
             "Data Publicatiei" to item.select("pubDate").text()
@@ -21,13 +22,13 @@ fun procesareJsoup(url: String): MutableMap<String, MutableMap<String, String>> 
 }
 
 fun main() {
-    val url = "http://rss.cnn.com/rss/edition.rss"
-    val lista = procesareJsoup(url)
-    lista.forEach {
-        println("${it.key}: ")
+    val url = "http://rss.cnn.com/rss/edition.rss" //url
+    val lista = procesareJsoup(url) //lista cu prelucrarea url ului
+    lista.forEach { //parcurge fiecare element din lista si afiseaza informatiile
+        println("${it.key}: ") 
         println("Link: ${it.value["Link"]}")
         println("Descriere: ${it.value["Descriere"]}")
         println("Data Publicatiei: ${it.value["Data Publicatiei"]}")
-        println() // Add a blank line for better readability
+        println() 
     }
 }
