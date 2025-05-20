8282dc8cf3addd29cd6f1d5329a9fc4c13a51f85
DariaAdelinne <157800209+DariaAdelinne@users.noreply.github.com>
2025-05-20 09:16:19 +0300

    Update main.py

--- Lab11/p1/main.py
+++ Lab11/p1/main.py
@@ -1,9 +1,9 @@
 import asyncio
 
 async def worker(name: str, queue: asyncio.Queue):
-    n = await queue.get()
+    n = await queue.get() #asteapta pana cand poate prelua un element din coada
     total = sum(range(n + 1))
-    print(f"{name} a calculat suma 0..{n} = {total}")
+    print(f"{name} a calculat suma 0..{n} = {total}") #afiseaza rezultatul impreuna cu numele corutinei
     queue.task_done()
 
 async def main():
@@ -13,15 +13,15 @@ async def main():
     for n in valori_n:
         await queue.put(n)
 
-    tasks = [
-        asyncio.create_task(worker(f"corutina-{i+1}", queue))
+    tasks = [ #cream o ista de corutine,una pentru fiecare valoare
+        asyncio.create_task(worker(f"corutina-{i+1}", queue)) #lanseaza in executie
         for i in range(len(valori_n))
     ]
 
-    await queue.join()
+    await queue.join() #asteapta pana cand toate sarcinile din coada s au terminat
 
     for t in tasks:
-        t.cancel()
+        t.cancel() #anulam toate corutinele ca sa nu ramana in asteptare
 
 if __name__ == "__main__":
     asyncio.run(main())
